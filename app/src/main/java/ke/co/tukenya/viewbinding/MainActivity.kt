f608a131ac17b1cbf3b0ef9ae49cf6bad80256c3
pekstaar <pekstaar@gmail.com>
2022-03-03 09:10:15 +0300

    Update

--- app/src/main/java/ke/co/tukenya/viewbinding/MainActivity.kt
+++ app/src/main/java/ke/co/tukenya/viewbinding/MainActivity.kt
@@ -18,5 +18,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(this.binding.root)
         this.viewModel=ViewModelProvider(this).get(StudentViewModel::class.java)
 
+        this.binding.button.setOnClickListener{
+val student = this.viewModel.getNext()
+            this.binding.txtViewStudent.text="Name:${student.name} Admin Number:${student.adm}"
+        }
     }
+
+
 }
